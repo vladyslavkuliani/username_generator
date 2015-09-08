006c5f0523d889daa50c1ea53378b653088836bc
nathanallen <thenathanator@gmail.com>
2015-09-08 16:12:35 -0700

    v2

--- username.rb
+++ username.rb
@@ -1,3 +1,6 @@
-def generate_username1(name)
-  "nothing here yet"
+# Make sure to run the tests in your /spec folder
+# Run `rspec /spec/username_spec_level_1.rb` to get started.
+
+def generate_username
+  # YOUR CODE HERE
 end
