60082c9af446c44f155170129b8b43022c4c58cd
nathanallen <thenathanator@gmail.com>
2015-09-08 17:14:36 -0700

    fix test

--- spec/username_spec_level_5.rb
+++ spec/username_spec_level_5.rb
@@ -6,10 +6,10 @@ require "./username.rb"
 describe "#generate_username, level 5" do
 
   it "it increments usernames that are already in use (uniqueness constraint)" do
-    expect( generate_username5("john", "doe", 1945) ).to eq "jdoe45"
-    expect( generate_username5("john", "doe", 1945) ).to eq "jdoe45_1"
-    expect( generate_username5("john", "doe", 1945) ).to eq "jdoe45_2"
-    expect( generate_username5("john", "doe", 1945) ).to eq "jdoe45_3"
+    expect( generate_username("john", "doe", 1945) ).to eq "jdoe45"
+    expect( generate_username("john", "doe", 1945) ).to eq "jdoe45_1"
+    expect( generate_username("john", "doe", 1945) ).to eq "jdoe45_2"
+    expect( generate_username("john", "doe", 1945) ).to eq "jdoe45_3"
   end
 
 end
