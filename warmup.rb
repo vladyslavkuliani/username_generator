d9dff34f8e54fd5872bd4da2904a48f7857b3bf2
nathanallen <thenathanator@gmail.com>
2015-09-09 09:31:51 -0700

    solution

--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,21 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase + "!"
 end
 
-def first_char
-  "z"
+def first_char(word)
+  word[0].downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(word)
+  word * 3
+end
+
+def after_you
+  "no, after you"
 end
