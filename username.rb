7af017510ce3db9826f9e621a498a766b4cd37fb
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-12-09 13:53:11 -0800

    warmup + username

--- username.rb
+++ username.rb
@@ -1,14 +1,44 @@
 # Make sure to run the tests in your /spec folder
 # Run `rspec /spec/username_spec.rb` to get started.
-
+$user_type = ["user", "seller", "manager", "admin"]
+$user_names = []
 def format_name(first, last)
-  nil
+  return nil if first.size==0 || last.size==0
+  first.gsub!(/\W|\d/, "")
+  last.insert(0, first.match(/^\w/).to_s).downcase.gsub(/\W|\d/, "")
 end
 
-def format_year
-  nil
+def format_year(year)
+  return nil if year.to_s.size!=4
+  year.to_s.match(/.{2}$/).to_s
 end
 
-def build_username
-  nil
+def check_privilege(index=0)
+  index = 3 if index>4
+  $user_type[index.floor]
+end
+
+def user_type_prefix(index=1)
+  return "" if index>=0 && index<1
+  check_privilege(index) + "-"
+end
+
+def build_username(first, last, year, index=0)
+  username = user_type_prefix(index)+format_name(first, last) + format_year(year)
+  username
+end
+
+def generate_username(first, last, year)
+  newName = build_username(first, last, year)
+  while $user_names.include?(newName) do
+    change_part = newName.match(/_.+$/).to_s
+    if change_part.size != 0
+      version = change_part[1..-1].to_i
+      newName.gsub!(change_part, "_"+(version+1).to_s)
+    else
+      newName += "_1"
+    end
+  end
+  $user_names << newName
+  newName
 end
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,27 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase!
+  message+="!"
 end
 
-def first_char
-  "z"
+def first_char(word)
+  letter = word.match(/^\w/).to_s
+  letter.downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(word)
+  str=""
+  3.times do
+    str += word.match(/#{word}/).to_s
+  end
+  str
+end
+
+def after_you
+  "no, after you"
 end
